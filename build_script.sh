92847833a5298be265eb0fb98904932285795729
Pyokyeong Son <pyoky3289@gmail.com>
2018-10-29 14:13:52 +0900

    Uploaded Gallery

--- build_script.sh
+++ build_script.sh
@@ -3,5 +3,9 @@
 
 (cd `dirname $0`/jekyll_blog && JEKYLL_ENV=production jekyll build)
 
+#(cd `dirname $0`/photor && JEKYLL_ENV=production jekyll build)
+
 (cd `dirname $0`/css && sass main.scss main.css)
+
+(cd `dirname $0`/css && sass gallery.scss gallery.css)
 
--- projects/axioms/ESXi_Power_On_Windows-10-VM.command
+++
@@ -1,1 +0,0 @@
-ssh root@192.168.11.102 vim-cmd vmsvc/power.on 11
